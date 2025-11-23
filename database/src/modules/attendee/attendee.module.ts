b1d6ead6040327c050eeef10e16bc8b1bb6c94f7
cedricnator <Cedric.kirmayr@gmail.com>
2025-11-23 00:54:58 -0300

    feat[attendee]: implement the minimal code for passing the tests

--- database/src/modules/attendee/attendee.module.ts
+++ database/src/modules/attendee/attendee.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AttendeeService } from './attendee.service';
 import { AttendeeController } from './attendee.controller';
+import { AttendeeEventService } from './attendee-event.service';
+import { AttendeeEventController } from './attendee-event.controller';
 
 @Module({
   imports: [],
-  providers: [AttendeeService],
-  exports: [AttendeeService],
-  controllers: [AttendeeController],
+  providers: [AttendeeService, AttendeeEventService],
+  exports: [AttendeeService, AttendeeEventService],
+  controllers: [AttendeeController, AttendeeEventController],
 })
 export class AttendeeModule {}
