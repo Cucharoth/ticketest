88079c951827adf1942f66d0f38da9fa0db66f21
cedricnator <Cedric.kirmayr@gmail.com>
2025-11-22 23:13:54 -0300

    feat[test]: added health controller test

---
+++ database/src/modules/health/health.controller.spec.ts
@@ -0,0 +1,22 @@
+import { TestingModule, Test } from '@nestjs/testing';
+
+describe('HealthController', () => {
+  let controller: HealthController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [HealthController],
+    }).compile();
+
+    controller = module.get<HealthController>(HealthController);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+
+  it('should check health', () => {
+    const result = controller.healthCheck();
+    expect(result).toEqual({ status: 'OK' });
+  });
+});
